b5f216da0577173479cef060c14064379c564a26
higebobo <higebobo@gmail.com>
2021-02-06 18:21:21 +0900

    modified

---
+++ pages/Android/_vendor.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <h1>{{ $route.params.vendor }}</h1>
+    <ul v-if="phoneData[$route.params.vendor]">
+      <li v-for="phone in phoneData[$route.params.vendor].phones"
+        v-bind:key="phone.id">
+        {{ phone.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      phoneData: {
+        Samsung: {
+          phones: [
+            { id: 1, name: 'Galaxy Note10+' },
+            { id: 2, name: 'Galaxy A20' },
+            { id: 3, name: 'Galaxy Fold' }
+          ]
+        },
+        SHARP: {
+          phones: [
+            { id: 1, name: 'AQUOS sense3' },
+            { id: 2, name: 'AQUOS sense3 lite' },
+            { id: 3, name: 'AQUOS sense3 plus' },
+            { id: 4, name: 'AQUOS zero2' }
+          ]
+        }
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.container {
+  margin: 10px;
+}
+</style>
---
+++ pages/_vendor/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1>{{ $route.params.vendor }}</h1>
+    <ul v-if="phoneData[$route.params.vendor]">
+      <li v-for="phone in phoneData[$route.params.vendor].phones" v-bind:key="phone.id">
+        {{ phone.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      phoneData: {
+        Apple: {
+          phones: [
+            { id: 1, name: 'iPhone 11' },
+            { id: 2, name: 'iPhone 11 Pro' },
+            { id: 3, name: 'iPhone 11 Pro Max' }
+          ]
+        }
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.container {
+  margin: 10px;
+}
+</style>
