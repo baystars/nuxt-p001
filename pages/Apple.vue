7461af4829363b281a921c6a64f2edc5418b0daa
higebobo <higebobo@gmail.com>
2021-02-06 18:03:35 +0900

    modified

---
+++ pages/Apple.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Apple</h1>
+    <ul>
+      <li>iPhone 11</li>
+      <li>iPhone 11 Pro</li>
+      <li>iPhone 11 Pro Max</li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+.container {
+  margin: 10px;
+}
+</style>
