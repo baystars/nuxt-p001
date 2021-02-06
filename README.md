893d72cf1202e73132522508b979d52e477a267f
higebobo <higebobo@gmail.com>
2021-02-06 16:27:17 +0900

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# p001-cli
+
+[「Vue\.js」でWebページを作成できるフレームワーク「Nuxt\.js」最初の一歩 \(1/3\)：CodeZine（コードジン）](https://codezine.jp/article/detail/11566)
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello {{name}}!</h1> <!-- nameの表示 …（1） -->
+    <input v-model="name"><!-- nameの入力 …（2）-->
+  </div>
+</template>
+<script>
+export default {
+  data() { // ページに表示するデータの指定 …（3）
+    return {
+      name: 'World' // nameの初期値 …（4）
+    }
+  }
+}
+</script>
