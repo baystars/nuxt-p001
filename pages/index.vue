0fec60768ad47efb9be9f498f53318002422e07a
higebobo <higebobo@gmail.com>
2021-02-08 16:36:31 +0900

    modified

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>
-    <h1>Hello {{name}}!</h1> <!-- nameの表示 …（1） -->
-    <input v-model="name"><!-- nameの入力 …（2）-->
+  <div class="container">
+    <h1>基本的なコンポーネントの例</h1>
+    <Apple/><!-- Appleコンポーネント -->
+    <Samsung/><!-- Samsungコンポーネント -->
   </div>
 </template>
 <script>
+// コンポーネントを参照
+import Apple from '~/components/Apple'
+import Samsung from '~/components/Samsung'
+// ページ実装部
 export default {
-  data() { // ページに表示するデータの指定 …（3）
-    return {
-      name: 'World' // nameの初期値 …（4）
-    }
+  // 利用するコンポーネントを定義
+  components: {
+    Apple,
+    Samsung
   }
 }
 </script>
+<style scoped>
+.container {
+  margin: 10px;
+}
+</style>
