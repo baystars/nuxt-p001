18ae048e1ed23269a09032a2c3dbdfccf66e3236
higebobo <higebobo@gmail.com>
2021-02-08 18:02:23 +0900

    modified

---
+++ components/InputBasic.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="component">
+    <!-- テキストボックス -->
+    氏名：<input v-model="name" v-on:input="onInput">
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: ''  // テキストボックスの入力内容
+    }
+  },
+  methods: {
+    onInput() { // テキストボックス変更時の処理
+      this.$store.commit('setName', this.name) // ミューテーション
+    }
+  }
+}
+</script>
+<style scoped>
+.component {
+  background-color: #ffaacc;
+  border: 1px solid black;
+  padding: 10px;
+}
+</style>
