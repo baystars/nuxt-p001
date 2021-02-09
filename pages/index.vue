bf1b4c42ec803f6dd8f59a3baf1cd6f3f48010ec
higebobo <higebobo@gmail.com>
2021-02-09 16:19:19 +0900

    modified

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-    <h1>Hello {{name}}!</h1> <!-- nameの表示 …（1） -->
-    <input v-model="name"><!-- nameの入力 …（2）-->
+  <div class="container">
+    <div>ページ表示日時：{{ new Date() }}</div> <!-- 日時を表示 -->
+    <h1>2020年秋冬注目スマホ</h1>
+    <!-- vendors配列からリストを表示 -->
+    <ul>
+      <li v-for="vendor in vendorsData" v-bind:key="vendor.id">
+        <nuxt-link v-bind:to="vendor.link">{{ vendor.name }}</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
-  data() { // ページに表示するデータの指定 …（3）
+  data() {
     return {
-      name: 'World' // nameの初期値 …（4）
+      // メーカーのリスト
+      vendorsData: [
+        { id: 1, name: 'Apple', link: '/phones/Apple' },
+        { id: 2, name: 'Google', link: '/phones/Google' },
+        { id: 3, name: 'Samsung', link: '/phones/Samsung' },
+      ]
     }
   }
 }
 </script>
+<style scoped>
+.container {
+  margin: 10px;
+}
+</style>
